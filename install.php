83e2c661ad37a63acc365ab7be4686f8f40be202
aeberhard <andreaseberhard@gmail.com>
2018-06-23 00:39:17 +0200

    Initial commit

---
+++ install.php
@@ -0,0 +1,7 @@
+<?php
+
+// Standard-Werte setzen
+
+if (!$this->hasConfig()) {
+    $this->setConfig('properties_settings', "# REDAXO-Properties setzen\n# PREFIX = my_\n\nHalloText = Servus Welt!\n");
+}
